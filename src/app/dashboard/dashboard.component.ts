8de8b07646f734e92759e1c0e9531289282f7dee
Rabbani <mohammad-rabbani@mercer.com>
2020-01-31 16:21:30 +0530

    updated register

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class DashboardComponent implements OnInit {
 
   constructor() { }
-
+  categories=['business','entertainment', 'general','health','science', 'sports', 'technology'];
   ngOnInit() {
   }
 
